6096688ac096618e07c84a63e867d4558bf82ddf
EPLCLooser <lucas@norrflod.se>
2025-03-07 14:28:04 +0100

    Fixat lite med menu button men har problem med utssendet av iconen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-function Menu(){
-    let 
+let menuButton = document.querySelector(".menu-button")
+let layer = document.querySelector(".fade-layer")
+
+menuButton.addEventListener("click",showMenu)
+layer.addEventListener("click",showMenu)
+
+function showMenu(){
+    let menu = document.querySelector(".menu")
+    let layer = document.querySelector(".fade-layer")
+
+    menu.classList.toggle("show")
+
+    layer.classList.toggle("visible")
 }
