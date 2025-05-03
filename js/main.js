290344baec7b35de42d5be7c8f71f1464c6a3132
EPLCLooser <lucas@norrflod.se>
2025-05-03 13:57:17 +0200

    hemma jobb

--- js/main.js
+++ js/main.js
@@ -13,3 +13,46 @@ function showMenu(){
     menu.classList.toggle("show")
     layer.classList.toggle("visible")
 }
+
+const images = [
+    'img/lenin-estrada-OI1ToozsKBw-unsplash.jpg',
+    'img/kenny-eliason-38XhGPwzI3U-unsplash.jpg'
+]
+  
+
+const bubblelist = document.querySelectorAll(".om_mig_bubblor")
+if (bubblelist) {
+    for (let i = 0; i < bubblelist.length; i++) {
+        const text = bubblelist[i].getAttribute("data-text")
+        bubblelist[i].addEventListener("click", () => showPopup(text))
+    }
+}
+function showPopup(text) {
+    const existingPopup = document.querySelector('.popup');
+    if (existingPopup) {
+        existingPopup.remove();
+    }
+    let popup = document.createElement('div')
+    popup.classList.add('popup')
+
+    popup.innerHTML = 
+    `<p>${text}</p>
+    
+    <button class="close-popup">Close</button>`
+
+    document.body.appendChild(popup)
+    popup.querySelector('.close-popup').addEventListener('click', () => {
+        popup.remove()
+    })
+}
+
+let currentIndex = 0
+const slideshow = document.getElementById('slideshow')
+if (slideshow) {
+    slideshow.addEventListener("click", nextimg)
+}
+    
+function nextimg(){
+    currentIndex = (currentIndex + 1) % images.length
+    slideshow.src = images[currentIndex]
+}
